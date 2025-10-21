b4cdfa8481e7b8f3d9b951574894f3858fda213d
lou12de39r <lou12de39r@gmail.com>
2025-10-21 10:16:42 +0900

    Fix keymap by replacing duplicate KC_SCLN with KC_S

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/keymap.c
@@ -8,7 +8,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_SCLN, KC_SCLN,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_S, KC_SCLN,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_ESC, KC_SCLN,    KC_H,    KC_J,    KC_K,    KC_L, KC_MINS,  KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_TAB,  KC_SCLN,   KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP,   MO(1),
         KC_LCTL, KC_LGUI, KC_LALT,KC_LNG1,    KC_SPC,    KC_LNG2,   KC_ENT,    KC_BSPC, KC_LNG2, KC_LEFT, KC_DOWN, KC_RGHT
