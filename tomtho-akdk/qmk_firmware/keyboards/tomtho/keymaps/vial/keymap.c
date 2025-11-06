ccc2f38d9474f56238cd8fb8c9efd1a16f0ac6f5
lou12de39r <lou12de39r@gmail.com>
2025-11-06 10:43:58 +0900

    Update keymap.c

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/keymap.c
@@ -2,13 +2,19 @@
 #include QMK_KEYBOARD_H
 #include "keymap_japanese.h" // 日本語配列キーコードを有効化
 
+// カスタムタッピングマクロの定義
+enum tap_dance_codes {
+    TD_WIN_D = 0, // TD(0) に対応
+    TD_Q_F2 = 1,  // TD(2) に対応（コード内では TD(2) がこの番号に対応するようにします）
+};
+
 // メインのキーマップ定義
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-		KC_ESC, TD(2), KC_W, KC_E, KC_R, KC_T, KC_7, KC_8, KC_9, KC_Y, KC_U, KC_I, KC_O, LT(3, KC_P),
+		KC_ESC, TD(TD_Q_F2), KC_W, KC_E, KC_R, KC_T, KC_7, KC_8, KC_9, KC_Y, KC_U, KC_I, KC_O, LT(3, KC_P),
 		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_4, KC_5, KC_6, KC_H, KC_J, KC_K, KC_L, KC_MINS,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_1, KC_2, KC_3, KC_N, KC_M, KC_COMM, KC_UP, RSFT(KC_SLSH),
-		KC_LCTL, TD(0), KC_LOPT, LT(1, KC_CAPS), LT(2, KC_SPC), LT(3, KC_0), KC_DOT, KC_BSPC, LT(1, KC_ENT), KC_LEFT, KC_DOWN, KC_RGHT
+		KC_LCTL, TD(TD_WIN_D), KC_LOPT, LT(1, KC_CAPS), LT(2, KC_SPC), LT(3, KC_0), KC_DOT, KC_BSPC, LT(1, KC_ENT), KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	[1] = LAYOUT(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), KC_TRNS, KC_SCLN, KC_QUOT, KC_LBRC, KC_INT3,
@@ -30,3 +36,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	)
 };
 
+// --- タップダンス定義 ---
+
+// TD(TD_WIN_D) の定義: タップで Win、ホールドで Win+D
+void td_win_d_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->interrupted || state->count == 0) {
+        // 何もしない
+    } else if (state->pressed) {
+        // ホールド: Win + D
+        register_code(KC_LGUI);
+        register_code(KC_D);
+    } else {
+        // タップ: Win
+        tap_code(KC_LGUI);
+    }
+}
+
+void td_win_d_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // ホールドの場合に押された Win+D をリリース
+    if (state->pressed) {
+        unregister_code(KC_D);
+        unregister_code(KC_LGUI);
+    }
+}
+
+// TD(TD_Q_F2) の定義: タップで Q、ダブルタップで F2
+void td_q_f2_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->interrupted || state->count == 0) {
+        // 何もしない
+    } else if (state->count == 1) {
+        // シングルタップ: Q
+        tap_code(KC_Q);
+    } else if (state->count == 2) {
+        // ダブルタップ: F2
+        tap_code(KC_F2);
+    }
+}
+
+void td_q_f2_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // 何もしない
+}
+
+// タップダンスの配列
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_WIN_D] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_win_d_finished, td_win_d_reset),
+    [TD_Q_F2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_q_f2_finished, td_q_f2_reset),
+};
