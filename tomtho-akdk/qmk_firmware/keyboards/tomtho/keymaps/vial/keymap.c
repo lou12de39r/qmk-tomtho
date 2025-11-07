8489f5cf102bcf31146b05a96c3dbeec9431f765
lou12de39r <lou12de39r@gmail.com>
2025-11-07 15:05:57 +0900

    keymap.c の更新

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/keymap.c
@@ -36,13 +36,11 @@ enum tap_dances {
 };
 
 // --- 関数の前方宣言 ---
-// これを追加することで、コンパイラが関数を認識できるようになります
 void td_gui_d_finished (tap_dance_state_t *state, void *user_data);
 void td_gui_d_reset (tap_dance_state_t *state, void *user_data);
 
 
 // タップダンスの実行時に呼ばれる関数を定義
-// 型名を qk_tap_dance_state_t から tap_dance_state_t に変更
 void td_gui_d_finished (tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
         // シングルタップの場合: 左Winキー (LGUI) を押す
@@ -54,7 +52,6 @@ void td_gui_d_finished (tap_dance_state_t *state, void *user_data) {
     }
 }
 
-// 型名を qk_tap_dance_state_t から tap_dance_state_t に変更
 void td_gui_d_reset (tap_dance_state_t *state, void *user_data) {
     // タップダンス終了時に押されたキーを離す処理
     unregister_code(KC_LGUI);
@@ -62,8 +59,7 @@ void td_gui_d_reset (tap_dance_state_t *state, void *user_data) {
 }
 
 // タップダンスの定義配列
-// 型名を qk_tap_dance_action_t から tap_dance_action_t に変更
-tap_dance_action_t tap_dance_actions[] = { // 変数名も qk_tap_dance_actions から tap_dance_actions に変更
+tap_dance_action_t tap_dance_actions[] = {
     [TD_LGUI_D] = ACTION_TAP_DANCE_FN_ADVANCED(td_gui_d_finished, NULL, td_gui_d_reset),
 };
 
@@ -72,25 +68,25 @@ tap_dance_action_t tap_dance_actions[] = { // 変数名も qk_tap_dance_actions 
 // メインのキーマップ定義
 // --------------------------------------------------
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-        = LAYOUT(
+        [0] = LAYOUT( // <-- ここから余分な = を削除
                 KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_7, KC_8, KC_9, KC_Y, KC_U, KC_I, KC_O, LT(3, KC_P),
                 KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_4, KC_5, KC_6, KC_H, KC_J, KC_K, KC_L, KC_MINS,
                 KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_1, KC_2, KC_3, KC_N, KC_M, KC_COMM, KC_UP, RSFT(KC_SLSH),
                 KC_LCTL, TD(TD_LGUI_D), KC_LOPT, LT(1, KC_CAPS), LT(2, KC_SPC), LT(3, KC_0), KC_DOT, KC_BSPC, LT(1, KC_ENT), KC_LEFT, KC_DOWN, KC_RGHT
         ),
-        = LAYOUT(
+        [1] = LAYOUT( // <-- ここから余分な = を削除
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), KC_TRNS, KC_SCLN, KC_QUOT, KC_LBRC, KC_INT3,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), KC_TRNS, LSFT(KC_8), LSFT(KC_9), KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         ),
-        = LAYOUT(
+        [2] = LAYOUT( // <-- ここから余分な = を削除
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_7, KC_8, KC_9, KC_PPLS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_F11, KC_4, KC_5, KC_6, KC_PMNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F12, KC_1, KC_2, KC_3, KC_PSLS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_0, KC_PDOT, KC_PAST
         ),
-        = LAYOUT(
+        [3] = LAYOUT( // <-- ここから余分な = を削除
                 QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_TRNS, KC_END, KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RCTL(KC_PGUP), KC_HOME, KC_TRNS, KC_END, RCTL(KC_PGDN),
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
