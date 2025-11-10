dbf036660245d0202355d09714d02f14505f780f
lou12de39r <lou12de39r@gmail.com>
2025-11-11 05:16:22 +0900

    Update config.h

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/config.h
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0x8f, 0x95, 0x3d, 0xb7, 0x6e, 0x83, 0xb5, 0x5d}
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
+// Combo と Tap Dance を使用する
+#define COMBO_COUNT 32            // 追加予定も考慮して余裕を持たせる
+#define TAP_DANCE_ENTRIES 32
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
