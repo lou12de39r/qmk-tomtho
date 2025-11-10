8be972b898ef3855979403285384b2f58a637cbc
lou12de39r <lou12de39r@gmail.com>
2025-11-11 02:56:46 +0900

    config.h の作成

---
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "config_common.h"
+
+// USB Device descriptor parameter
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Auto-Keyboard-Design-Kit
+#define PRODUCT         tomtho
+
+// Matrix size
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 14
+
+// Diode direction
+#define DIODE_DIRECTION COL2ROW
