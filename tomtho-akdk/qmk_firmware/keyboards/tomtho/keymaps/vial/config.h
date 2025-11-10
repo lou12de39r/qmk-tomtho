11c492db2c27a2aacdb357114836f69db6377aa0
lou12de39r <lou12de39r@gmail.com>
2025-11-11 04:38:42 +0900

    Update config.h

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/config.h
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/vial/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x8f, 0x95, 0x3d, 0xb7, 0x6e, 0x83, 0xb5, 0x5d}
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
+#define VIAL_TAP_DANCE_ENTRIES 32
+#define VIAL_COMBO_ENTRIES 32
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
