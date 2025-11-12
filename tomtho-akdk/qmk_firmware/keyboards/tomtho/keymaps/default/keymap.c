da0a9a6533824d971696ef31a1b429818455ddfc
lou12de39r <lou12de39r@gmail.com>
2025-11-12 10:56:59 +0900

    Update keymap.c

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
@@ -128,10 +128,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // ==========================================================
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        TD(TD_ESC_CAPS), TD(TD_Q_LAYER3), KC_W, KC_E, KC_R, KC_T, KC_7, KC_8, KC_9, KC_Y, KC_U, KC_I, KC_O, LT(4,KC_P),
+        KC_ESC, TD(TD_Q_LAYER3), KC_W, KC_E, KC_R, KC_T, KC_7, KC_8, KC_9, KC_Y, KC_U, KC_I, KC_O, LT(4,KC_P),
         KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_4, KC_5, KC_6, KC_H, KC_J, KC_K, KC_L, KC_MINS,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_1, KC_2, KC_3, KC_N, KC_M, KC_COMM, KC_UP, MT(MOD_LSFT, KC_SLSH),
-        KC_LCTL, TD(TD_LGUI_D), KC_LOPT, TD(TD_DOT_CAPS), LT(2, KC_SPC), LT(3,KC_0), KC_DOT, KC_BSPC, LT(1, KC_ENT), KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, TD(TD_LGUI_D), KC_LOPT, KC_CAPS, LT(2, KC_SPC), LT(3,KC_0), KC_DOT, KC_BSPC, LT(1, KC_ENT), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
@@ -151,8 +151,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT(
         KC_TRNS, KC_TRNS, MS_BTN4, KC_TRNS, MS_BTN5, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, MS_UP, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, MS_BTN1, MS_BTN3, MS_BTN2, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_F11, MS_LEFT, MS_DOWN, MS_RGHT, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS, KC_TRNS, KC_WH_U, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HW_L, KC_HW_D, KC_HW_R
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS, KC_TRNS, WH_U, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, HW_L, HW_D, HW_R
     ),
 
     [4] = LAYOUT(
