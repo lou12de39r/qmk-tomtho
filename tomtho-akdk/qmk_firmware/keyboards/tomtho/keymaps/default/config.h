65b4d73ffbf626ba8c529272f662b32f34dfba96
lou12de39r <lou12de39r@gmail.com>
2025-11-12 10:26:12 +0900

    Update config.h

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/config.h
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/config.h
@@ -4,3 +4,5 @@
 #define COMBO_COUNT 32            // 追加予定も考慮して余裕を持たせる
 #define TAP_DANCE_ENTRIES 32
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
+// タップを優先する（キーをすぐ離したとき）
+#define TAPPING_FORCE_HOLD
