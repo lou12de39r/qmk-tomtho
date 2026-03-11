e516bd8e3b8e234bd3551944a478be4b1a54bb1c
lou12de39r <lou12de39r@gmail.com>
2026-03-11 10:27:09 +0900

    Remove unused combo actions and update combos

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
@@ -38,9 +38,7 @@ enum combo_events {
     COMBO_UNDS,
     COMBO_COLN,
     COMBO_SCLN,
-    COMBO_BRACKET,
-    COMBO_PAREN,
-    COMBO_BRACE
+    COMBO_PAREN
 };
 
 const uint16_t PROGMEM del_combo[] = {KC_DOWN, KC_RGHT, COMBO_END};
@@ -49,13 +47,9 @@ const uint16_t PROGMEM op_screenshot_combo[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM unds_combo[] = {KC_L, KC_MINS, COMBO_END};
 
 const uint16_t PROGMEM coln_combo[] = {KC_9, KC_Y, COMBO_END};
-const uint16_t PROGMEM scln_combo[] = {KC_8, KC_H, COMBO_END};
+const uint16_t PROGMEM scln_combo[] = {KC_6, KC_H, COMBO_END};
 
-// ★ 入れ替え
-const uint16_t PROGMEM bracket_combo[] = {KC_U, KC_I, COMBO_END};
-const uint16_t PROGMEM paren_combo[]   = {KC_J, KC_K, COMBO_END};
-
-const uint16_t PROGMEM brace_combo[]   = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM paren_combo[] = {KC_J, KC_K, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     [COMBO_DEL]            = COMBO(del_combo, KC_DEL),
@@ -66,9 +60,7 @@ combo_t key_combos[COMBO_COUNT] = {
     [COMBO_COLN]           = COMBO(coln_combo, JP_COLN),
     [COMBO_SCLN]           = COMBO(scln_combo, KC_SCLN),
 
-    [COMBO_BRACKET]        = COMBO_ACTION(bracket_combo),
     [COMBO_PAREN]          = COMBO_ACTION(paren_combo),
-    [COMBO_BRACE]          = COMBO_ACTION(brace_combo),
 };
 
 // ==========================================================
@@ -80,24 +72,10 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 
     switch(combo_index) {
 
-        // []  ← U+I
-        case COMBO_BRACKET:
-            tap_code16(JP_LBRC);
-            tap_code16(JP_RBRC);
-            tap_code(KC_LEFT);
-            break;
-
-        // ()  ← J+K
+        // () ← J+K
         case COMBO_PAREN:
             tap_code16(JP_LPRN);
             tap_code16(JP_RPRN);
-            tap_code(KC_LEFT);
-            break;
-
-        // {}
-        case COMBO_BRACE:
-            tap_code16(JP_LCBR);
-            tap_code16(JP_RCBR);
             tap_code(KC_LEFT);
             break;
     }
