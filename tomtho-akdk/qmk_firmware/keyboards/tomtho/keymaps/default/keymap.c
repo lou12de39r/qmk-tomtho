2114215bf04e7c608b884cb5605258383084e294
lou12de39r <lou12de39r@gmail.com>
2026-03-24 13:52:24 +0900

    Refactor tap dance functions for Q key handling

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
@@ -10,7 +10,7 @@
 
 enum {
     TD_LGUI_D = 0,
-    TD_Q_WIN_TAB
+    TD_Q_SMART
 };
 
 // LGUI+D
@@ -27,21 +27,31 @@ void dance_lgui_d_reset(tap_dance_state_t *state, void *user_data) {
     unregister_code(KC_LGUI);
 }
 
-// Q double = Win+Tab (1回のみ)
-void dance_q_win_tab_finished(tap_dance_state_t *state, void *user_data) {
+// Q tap/hold/double
+void dance_q_finished(tap_dance_state_t *state, void *user_data) {
+
+    // HOLD → Alt+F4
+    if (state->pressed) {
+        tap_code16(A(KC_F4));
+        return;
+    }
+
+    // SINGLE TAP → q
     if (state->count == 1) {
         tap_code(KC_Q);
     }
+
+    // DOUBLE TAP → Win+Tab
     else if (state->count == 2) {
         tap_code16(G(KC_TAB));
     }
 }
 
-void dance_q_win_tab_reset(tap_dance_state_t *state, void *user_data) {}
+void dance_q_reset(tap_dance_state_t *state, void *user_data) {}
 
 tap_dance_action_t tap_dance_actions[] = {
     [TD_LGUI_D] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_lgui_d_finished, dance_lgui_d_reset),
-    [TD_Q_WIN_TAB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_q_win_tab_finished, dance_q_win_tab_reset),
+    [TD_Q_SMART] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_q_finished, dance_q_reset),
 };
 
 // ==========================================================
@@ -127,7 +137,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT(
-KC_ESC,TD(TD_Q_WIN_TAB),KC_W,KC_E,KC_R,KC_T,KC_7,KC_8,KC_9,KC_Y,KC_U,KC_I,KC_O,KC_P,
+KC_ESC,TD(TD_Q_SMART),KC_W,KC_E,KC_R,KC_T,KC_7,KC_8,KC_9,KC_Y,KC_U,KC_I,KC_O,KC_P,
 KC_TAB,KC_A,KC_S,KC_D,KC_F,KC_G,KC_4,KC_5,KC_6,KC_H,KC_J,KC_K,KC_L,KC_MINS,
 KC_LSFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_1,KC_2,KC_3,KC_N,KC_M,KC_COMM,KC_UP,MT(MOD_LSFT,KC_SLSH),
 KC_LCTL,TD(TD_LGUI_D),MT(MOD_LALT,KC_INT4),LT(4,KC_CAPS),LT(2,KC_SPC),LT(3,KC_0),KC_DOT,KC_BSPC,LT(1,KC_ENT),KC_LEFT,KC_DOWN,KC_RGHT
