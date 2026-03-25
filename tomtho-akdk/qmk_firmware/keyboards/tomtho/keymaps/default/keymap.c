dc74fca685e7276d78d2bcf6c853cdfcc177f809
lou12de39r <lou12de39r@gmail.com>
2026-03-26 08:25:03 +0900

    Update keymap comment for clarity

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
@@ -130,7 +130,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 // ==========================================================
-// Keymap
+// Keymap1
 // ==========================================================
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
