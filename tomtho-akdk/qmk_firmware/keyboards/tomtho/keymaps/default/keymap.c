87519f22cce8a2dceb1f71b91cae28f0d5b72a08
lou12de39r <lou12de39r@gmail.com>
2026-03-27 09:27:05 +0900

    Update screenshot_combo to use LT for key J

--- tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
+++ tomtho-akdk/qmk_firmware/keyboards/tomtho/keymaps/default/keymap.c
@@ -64,11 +64,14 @@ enum combo_events {
 
 const uint16_t PROGMEM del_combo[] = {KC_DOWN, KC_RGHT, COMBO_END};
 const uint16_t PROGMEM dot_combo[] = {KC_COMM, KC_UP, COMBO_END};
-const uint16_t PROGMEM screenshot_combo[] = {KC_J, KC_K, KC_L, COMBO_END};
+
+/* 修正：LT(5,KC_J) */
+const uint16_t PROGMEM screenshot_combo[] = {LT(5,KC_J), KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM paren_combo[] = {LT(5,KC_J), KC_K, COMBO_END};
+
 const uint16_t PROGMEM unds_combo[] = {KC_L, KC_MINS, COMBO_END};
 const uint16_t PROGMEM coln_combo[] = {KC_9, KC_Y, COMBO_END};
 const uint16_t PROGMEM scln_combo[] = {KC_6, KC_H, COMBO_END};
-const uint16_t PROGMEM paren_combo[] = {KC_J, KC_K, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     [COMBO_DEL] = COMBO(del_combo, KC_DEL),
@@ -142,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT(
 KC_ESC,TD(TD_Q_SMART),KC_W,KC_E,KC_R,KC_T,KC_7,KC_8,KC_9,KC_Y,KC_U,KC_I,KC_O,KC_P,
-KC_TAB,KC_A,KC_S,LT(5,KC_D),KC_F,KC_G,KC_4,KC_5,KC_6,KC_H,LT(5,KC_J),KC_K,KC_L,KC_MINS,
+KC_TAB,KC_A,KC_S,KC_D,LT(5,KC_F),KC_G,KC_4,KC_5,KC_6,KC_H,LT(5,KC_J),KC_K,KC_L,KC_MINS,
 KC_LSFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_1,KC_2,KC_3,KC_N,KC_M,KC_COMM,KC_UP,MT(MOD_LSFT,KC_SLSH),
 KC_LCTL,TD(TD_LGUI_D),MT(MOD_LALT,KC_INT4),LT(4,KC_CAPS),LT(2,KC_SPC),LT(3,KC_0),KC_DOT,KC_BSPC,LT(1,KC_ENT),KC_LEFT,KC_DOWN,KC_RGHT
 ),
